b1a32f5b515cdeba5ce7f6d41add91b4cfd4cbf1
Diptom Saha <sahadiptom128@gmail.com>
2021-06-16 13:04:13 +0530

    Added OPENAPI Documentation

--- src/main/java/com/example/demo/NewsServiceApplication.java
+++ src/main/java/com/example/demo/NewsServiceApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title="NewService",version="2.0",description="News webservice"))
 public class NewsServiceApplication {
 
 	public static void main(String[] args) {
