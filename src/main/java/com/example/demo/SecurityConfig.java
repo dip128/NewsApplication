ef5ac92aac773cc69990b0939a959ee526684ec5
Diptom Saha <sahadiptom128@gmail.com>
2021-07-08 22:31:59 +0530

    Secure EndPoint as per the documentations

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,38 +22,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
-	@Bean
-	public AuthenticationProvider authProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		
-		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(getPasswordEncoder());
-		return provider;
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
 	}
-	
-//	@Override
-//	protected UserDetailsService userDetailsService() {
-//		// TODO Auto-generated method stub
-//		List<UserDetails> users = new ArrayList<>();
-//		users.add(User.withDefaultPasswordEncoder().username("test").password("123").roles("ADMIN").build());
-//		return new InMemoryUserDetailsManager(users);
-//	}
 
-//	@Override
-//	public void configure(AuthenticationManagerBuilder auth) throws Exception{
-//		auth.inMemoryAuthentication()
-//		.withUser("test")
-//		.password("test")
-//		.roles("ADMIN");
-//	}
-//	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.csrf().disable()
 		.authorizeRequests()
+		.antMatchers("/news/add/**").authenticated()
 			.antMatchers("/**").permitAll()
-			.and().formLogin();
+			.and().httpBasic();
 		http.headers().frameOptions().disable();
 			
 			
