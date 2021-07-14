89025c66cde8f9e6b870d2a9dc2aac81c9debf36
Diptom Saha <sahadiptom128@gmail.com>
2021-07-14 18:59:08 +0530

    Cors Config Updated

--- src/main/java/com/example/demo/NewsServiceApplication.java
+++ src/main/java/com/example/demo/NewsServiceApplication.java
@@ -18,14 +18,14 @@ public class NewsServiceApplication {
 		SpringApplication.run(NewsServiceApplication.class, args);
 	}
 	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 
 }
