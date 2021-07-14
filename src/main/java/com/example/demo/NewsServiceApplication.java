ed3ebf81687a79a044b7f3610202ca6634811595
Diptom Saha <sahadiptom128@gmail.com>
2021-07-14 17:06:50 +0530

    Cors Config added

--- src/main/java/com/example/demo/NewsServiceApplication.java
+++ src/main/java/com/example/demo/NewsServiceApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -13,6 +16,16 @@ public class NewsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsServiceApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/demo/controller/CatagoryController.java
+++ src/main/java/com/example/demo/controller/CatagoryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.example.demo.entity.CatagoryEntity;
 import com.example.demo.repository.CatagoryRespository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class CatagoryController {
 	
 	@Autowired
